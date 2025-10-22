3596963a88db710b1035061ae0bf136621910def
Turbash <negirawatdeepi@gmail.com>
2025-10-22 16:27:24 +0530

    Main loop logic started image set as background

--- main.py
+++ main.py
@@ -16,14 +16,27 @@ OBJ_SIZE=5
 VEL_SCALE=100
 
 BG = pygame.transform.scale(pygame.image.load("background.jpg"),(WIDTH, HEIGHT))
-PLANET = pygame.transform.scale(pygame.image.load("planet.png"),(PLANET_SIZE*2,PLANET_SIZE*2))
+PLANET = pygame.transform.scale(pygame.image.load("jupiter.png"),(PLANET_SIZE*2,PLANET_SIZE*2))
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 
 def main():
-    pass
+    running = True
+    clock = pygame.time.Clock()
 
-if __name__ == ""__main__":
+    while(running):
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        win.blit(BG, (0, 0))
+        pygame.display.update()
+
+    pygame.quit()
+
+if __name__ == "__main__":
     main()
